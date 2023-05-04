ccbc6c1df5bb7309150e6ad9b8c41f8301d998eb
Zongyuan Li <lizongy1@msu.edu>
2023-05-04 05:01:41 -0400

    new

--- hello.cs
+++ hello.cs
@@ -3,6 +3,7 @@ namespace HelloWorld
     class Hello {         
         static void Main(string[] args)
         {
+            System.Console.WriteLine("hello world");
             System.Console.WriteLine("hello world 222");
         }
     }
